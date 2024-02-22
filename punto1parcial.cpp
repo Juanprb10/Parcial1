612d7b55e7891fac2b82132829f63bb6e0aba06f
Juanprb10 <159449923+Juanprb10@users.noreply.github.com>
2024-02-22 08:53:23 -0500

    Add files via upload

---
+++ punto1parcial.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int cantidad;
+    cout << "Ingrese cantidad de numeros: " << endl;
+    cin >> cantidad;
+    while (cantidad <= 0) {
+        cout << "--- ERROR ---" << endl;
+        cout << "La cantidad de numeros debe ser mayor a cero." << endl;
+        cout << "Ingrese cantidad de numeros: " << endl;
+        cin >> cantidad;
+    }
+    double suma = 0;
+    double prom;
+    double maximo, minimo;
+    cout << "Ingrese el numero 1: " << endl;
+    cin >> maximo;
+    suma += maximo;
+    minimo = maximo;
+    for (int i = 1; i < cantidad; ++i) {
+        double numero;
+        cout << "Ingrese el numero " << i + 1 << ": " << endl;
+        cin >> numero;
+        suma += numero;
+        if (numero > maximo) {
+            maximo = numero;
+        }
+        if (numero < minimo) {
+            minimo = numero;
+        }
+    }
+    prom = suma / cantidad;
+    cout << "\nResultados:" << endl;
+    cout << " Promedio: " << prom << endl;
+    cout << " Mayor: " << maximo << endl;
+    cout << " Menor: " << minimo << endl;
+    return 0;
+}
